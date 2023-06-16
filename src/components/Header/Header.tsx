4ff48cf81061b4c10bd6d4c87ea7182d2026e980
mutaikhan_almubdi <mutaikhanali@gmail.com>
2023-06-16 13:23:18 +0600

    set link to logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,12 @@ import './Header.css';
 const Header = () => {
   return (
     <header>
-      <div className="logo">
+      <div
+        className="logo"
+        onClick={() => {
+          window.location.replace('/');
+        }}
+      >
         <img
           src="https://cdn0.iconfinder.com/data/icons/digital-asset-1/512/Blockchain-bitcoin-cryptocurrency-cubes-crypto-block-network-digital-technology-64.png"
           alt="logo"
